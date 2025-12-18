aee1195d7fb6be81e73f9e718b0ab2ea42237208
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-12-18 16:58:19 +0530

    first commit!

---
+++ src/pages/home.rs
@@ -0,0 +1,32 @@
+use crate::components::counter_btn::Button;
+use leptos::prelude::*;
+
+/// Default Home Page
+#[component]
+pub fn Home() -> impl IntoView {
+    view! {
+        <ErrorBoundary fallback=|errors| {
+            view! {
+                <h1>"Uh oh! Something went wrong!"</h1>
+
+                <p>"Errors: "</p>
+                // Render a list of errors as strings - good for development purposes
+                <ul>
+                    {move || {
+                        errors
+                            .get()
+                            .into_iter()
+                            .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                            .collect_view()
+                    }}
+
+                </ul>
+            }
+        }>
+
+            <div class="container">
+                "Hi there, I'm Nilesh"
+            </div>
+        </ErrorBoundary>
+    }
+}
