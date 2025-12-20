84401f9b377aee806352f0221b8bcbbd87897158
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-12-21 02:01:46 +0530

    what

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,9 +1,26 @@
-use crate::components::counter_btn::Button;
+
 use leptos::prelude::*;
 
 /// Default Home Page
 #[component]
 pub fn Home() -> impl IntoView {
+    let dot_dot_dot = RwSignal::new(".  ");
+    let dark = RwSignal::new(true);
+    set_interval(
+        move || {
+            dot_dot_dot.update(|this| {
+                *this = match *this {
+                    ".  " => ".. ",
+                    ".. " => "...",
+                    "..." => "   ",
+                    "   " => ".  ",
+                    _ => panic!("Fucking panicked here")
+                };
+            });
+        },
+        std::time::Duration::from_secs(1),
+    );
+    
     view! {
         <ErrorBoundary fallback=|errors| {
             view! {
@@ -24,9 +41,31 @@ pub fn Home() -> impl IntoView {
             }
         }>
 
-            <div class="container">
-                "Hi there, I'm Nilesh"
+            <nav>
+                <label class= "theme-toggle">
+                    <input type="checkbox" on:click = move |_| dark.update(|x| *x = !*x)/>
+                    <svg
+                        viewBox="0 0 960 960"
+                        width="20"
+                        height="20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path class:morph-path = move || *dark.read() 
+                            class:morph-back-path = move || !*dark.read()
+                            fill="#e0def4" />
+                    </svg>
+                </label>
+                <button>
+                    <img src="public/github.png" />
+                </button>
+            </nav>
+            <main>
+            <div class="container">"This guy is onto something" {move || *dot_dot_dot.read()}
+            // {move || *dot_dot_dot.read()}
             </div>
+            </main>
         </ErrorBoundary>
     }
 }
+
+
